c2f1a6fa6dca3c095c2f8c5276162d501227fba1
gllys_letian <474040454@qq.com>
2015-03-23 14:21:19 +0800

    login

--- SoapUi/SoapUi/LogIn/LogIn.java
+++ SoapUi/SoapUi/LogIn/LogIn.java
@@ -1,33 +1,47 @@
 package SoapUi.LogIn;
 
-import org.openqa.selenium.*;
-import org.testng.*;
+import org.openqa.selenium.WebDriver;
 
 
-public class TestLogIn{
+import data_source.BrowersType;
+import data_source.browers_set;
+
+import DO_source.DO;
+//import wait_source.Wait;
+
+public class LogIn {
 	private WebDriver driver;
 	private DO du;
-	private Wait waiter;
-	
-	
-	public String LogIn(){
-		
-	
-		driver = new FirefoxDriver();
-		du = new DO du;
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+//	private Wait waiter;
+	private String Url = "www.piaotai.com";
+//
+//	public void BeforeClass()
+//
+//	{
+//
+//		browers_set browser = new browers_set(BrowersType.chrome);
+//		driver = browser.web_driver;
+//		du = new DO(driver);
+//
+//	}
+
+	public String LogInTest() {
+		browers_set browser = new browers_set(BrowersType.chrome);
+		driver = browser.web_driver;
+		du = new DO(driver);   
+		driver.get(Url);
+		du.what("username").clear();
+		du.what("username").sendKeys("gllysleletian");
+		du.what("password").sendKeys("0823letian");
+		du.what("submit").submit();
+		return du.what("title").getText();
+
+	}
+
+	public static void main(String arg[]) {
+       
+		System.out.println(new LogIn().LogInTest());
 		
 	}
-	
-	
+
 }
