620872ec86e86cae20190dae918963d6c86ab248
gllys_letian <474040454@qq.com>
2015-03-23 13:03:00 +0800

    all new

---
+++ SoapUi/SoapUi/LogIn/LogIn.java
@@ -0,0 +1,33 @@
+package SoapUi.LogIn;
+
+import org.openqa.selenium.*;
+import org.testng.*;
+
+
+public class TestLogIn{
+	private WebDriver driver;
+	private DO du;
+	private Wait waiter;
+	
+	
+	public String LogIn(){
+		
+	
+		driver = new FirefoxDriver();
+		du = new DO du;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
